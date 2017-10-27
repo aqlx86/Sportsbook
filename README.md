1029f48e8f2e36c7129c558ba0e67448644526fd
foxhound <aqlx86@gmail.com>
2017-10-27 16:06:44 +0800

    initial wip commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Sportsbook
+
 PHP Client for SportsBook API Service
+
+Currently coupled with laravel 5.
+
+
+## Todo
+* Log requests
---
+++ src/SportsBook/SportsBook.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace SportsBook;
+
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+
+class SportsBook
+{
+    const METHOD_DO_SETTLEMENT = 'DoSettlement';
+    const METHOD_CONFIRM_BET = 'ConfirmBet';
+    const METHOD_PUSH_EVENTS_INFO = 'PushEventsInfo';
+    const METHOD_PLACE_BET = 'PlaceBet';
+
+    public $wrap_form_data = true;
+
+    private $http_client;
+
+    protected $response;
+    protected $headers = [];
+    protected $default_params;
+    protected $form_params = [];
+    protected $data = null;
+
+    public function __construct()
+    {
+        $this->http_client = new Client([
+            'base_uri' => env('SPORTSBOOK_BASE_URI'),
+            'timeout'  => 3.0,
+        ]);
+
+        $this->default_params = [
+            'Vendor'     => env('SPORTSBOOK_VENDOR'),
+            'VendorKey'  => env('SPORTSBOOK_VENDOR_KEY'),
+            'TimeStamp'  => Carbon::now(),
+            'Seq'        => str_random(16).time(),
+        ];
+    }
+
+    public function set_data(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function append_form_params(array $params)
+    {
+        $this->form_params = array_merge($this->form_params, $params);
+    }
+
+    public function add_param($key, $value)
+    {
+        $this->form_params[$key] = $value;
+    }
+
+    public function add_header($key, $value)
+    {
+        $this->headers[$key] = $value;
+    }
+
+    public function post($method)
+    {
+        $this->prepare_params();
+
+        $this->response = $this->http_client->request('POST', $method, [
+            'json' => $this->form_params,
+            'headers' => $this->headers,
+        ]);
+
+        return $this;
+    }
+
+    public function response()
+    {
+        return json_decode($this->response->getBody(), true);
+    }
+
+    protected function prepare_params()
+    {
+        if ($this->data)
+            $this->form_params['Data'] = $this->data;
+
+        $this->form_params = array_merge($this->default_params, $this->form_params);
+    }
+
+}
