b047a29efeb968223e7f215d0feae63c31ae7a5e
Arnel Labarda <aqlx86@gmail.com>
2017-11-09 12:57:27 +0800

    fix odds type values c/o kent

--- src/SportsBook/OddsType.php
+++ src/SportsBook/OddsType.php
@@ -5,25 +5,26 @@ namespace SportsBook;
 class OddsType
 {
     // decimal odds
-    const TYPE_DEC = 1;
+    const TYPE_DEC = 16;
     // hk odds
-    const TYPE_HK = 2;
+    const TYPE_HK = 6;
     // indo odds
-    const TYPE_INDO = 3;
+    const TYPE_INDO = 11;
     // malay odds
-    const TYPE_MY = 4;
+    const TYPE_MY = 1;
 
+    // returns odd type id in sportsbook
     public static function get_cf_odds($odd_type)
     {
         // hard coded cf88 odds type, please refer to cf888.odds_types table
 
         if ($odd_type == self::TYPE_DEC)
-            return 16;
-        else if ($odd_type == self::TYPE_HK)
-            return 6;
-        else if ($odd_type == self::TYPE_INDO)
-            return 11;
-        else if ($odd_type == self::TYPE_MY)
             return 1;
+        else if ($odd_type == self::TYPE_HK)
+            return 2;
+        else if ($odd_type == self::TYPE_INDO)
+            return 3;
+        else if ($odd_type == self::TYPE_MY)
+            return 4;
     }
 }
